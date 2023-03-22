3143c15053b4c048bf4eace51fc83417f16e7e07
Iryna Oleksyshyn <irynkaoleksyshyn@gmail.com>
2023-03-22 21:29:38 +0200

    Add task 10

--- js/task-10.js
+++ js/task-10.js
@@ -3,3 +3,36 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const inputEl = document.querySelector('#controls > input');
+
+const createButton = document.querySelector('[data-create]');
+
+createButton.addEventListener('click', () => {
+  createBoxes(inputEl.value);
+});
+
+let elements = [];
+
+const boxesEl = document.querySelector('#boxes');
+
+function createBoxes(amount) {
+  for (let i = 0; i < amount; i += 1) {
+    const divEl = document.createElement('div');
+    divEl.style.backgroundColor = getRandomHexColor();
+    divEl.style.width = `${30 + 10 * i}px`;
+    divEl.style.height = `${30 + 10 * i}px`;
+    elements.push(divEl);
+  }
+  
+  boxesEl.append(...elements);
+}
+
+const destroyButton = document.querySelector('[data-destroy]');
+
+destroyButton.addEventListener('click', destroyBoxes);
+
+function destroyBoxes() {
+  boxesEl.innerHTML = '';
+  inputEl.value = '';
+}
