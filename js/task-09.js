2ecc3001dbcdf80fcf28fc88f639d998dcf25a9b
Iryna Oleksyshyn <irynkaoleksyshyn@gmail.com>
2023-03-22 16:16:26 +0200

    Add task 9

--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,13 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const colorEl = document.querySelector('.color');
+
+const buttonEl = document.querySelector('.change-color');
+
+buttonEl.addEventListener('click', () => {
+  colorEl.textContent = getRandomHexColor();
+  const bodyEl = document.querySelector('body');
+  bodyEl.style.backgroundColor = getRandomHexColor();
+});
