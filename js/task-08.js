47a363dde64ecca1b163767734e59f6f446ece2b
Iryna Oleksyshyn <irynkaoleksyshyn@gmail.com>
2023-03-22 15:20:22 +0200

    Add 7 and 8 tasks

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,26 @@
+const form = document.querySelector('.login-form');
 
+form.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(event) {
+    event.preventDefault();
+
+    const formElements = event.currentTarget.elements;
+    
+    const email = formElements.email.value;
+    const password = formElements.password.value;
+   
+    if (email === '' || password === '') {
+        const message = "All fields must be filled!";
+        alert(message);
+    } else {
+        const formData = {
+            email,
+            password,
+        };
+        
+        console.log(formData);
+        document.querySelector('.login-form').reset();
+    }
+    
+}
